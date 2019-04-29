189a670722f6eccb66bb9600f02db148cc485147
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-04-29 09:14:32 +0300

    adds a timer to be able to view the time a quote was published

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,3 +1,3 @@
 export class Quote {
-    constructor(public id:number, public name:string,public description:string,public submitter:string){}
+    constructor(public id:number, public name:string,public description:string,public submitter:string,public publish:Date){}
 }
