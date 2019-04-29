d3052087aa606bfb199d38d4700e8620e943a149
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-04-29 07:36:18 +0300

    adds funtionality to the submit button to enable the user to submit a quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -21,6 +21,11 @@ export class QuoteComponent implements OnInit {
         }
     }
 }
+  addNewQuote(quote){
+    let quoteLength =this.quotes.length;
+    quote.id=quoteLength+1;
+    this.quotes.push(quote);
+  }
 
   constructor() { }
 
