968c00551b712801b1385d78f488eb39d0299344
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-04-29 05:14:01 +0300

    nested the quote component within the app component and modified the quote component to have the content previously on the app component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Quote} from '../quote'
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes = [
+    new Quote (1, 'winter is here'),
+    new Quote (2, 'valar murgulis'),
+  ]
 
   constructor() { }
 
