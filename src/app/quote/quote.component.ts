8d3ec7515291785e50767114f8e3b0e7bcd005a0
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-04-29 05:38:21 +0300

    adds the funtionality of the delete button

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,6 +12,16 @@ export class QuoteComponent implements OnInit {
     new Quote (2, 'valar murgulis'),
   ]
 
+  deleteQuote(isComplete,index){
+    if (isComplete){
+        let toDelete=confirm(`Are you sure you want to delete ${this.quotes[index].name}`)
+        
+        if(toDelete){
+            this.quotes.splice(index,1)
+        }
+    }
+}
+
   constructor() { }
 
   ngOnInit() {
